f8b212816d37f69dd58c964c6b7b6ffd5b7ede5f
beyond1748 <141267854+beyond1748@users.noreply.github.com>
2026-02-17 18:22:42 +0100

    update test action

--- runscripts/test.py
+++ runscripts/test.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import os
+from datetime import datetime
 
 out_dir = Path("data/")
 out_dir.mkdir(parents=True, exist_ok=True)
 
-(out_dir / "test.txt").write_text("test action:tm:\n" + os.environ["data"])
+(out_dir / "test.txt").write_text("test action:tm:\n" + os.environ["data"] + f"\n{datetime.now().isoformat() + 'Z'}")
 print("mmokay")
